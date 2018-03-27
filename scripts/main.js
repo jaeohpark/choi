54d39a424133d33f8506747f88d2e3ddbbd3d5da
jaeoh <wodh4654@naver.com>
2018-03-27 11:56:28 +0900

    second

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,23 @@
-// Image switcher code
-
 var myHeading = document.querySelector('h1');
-myHeading.innerHTML = 'Cloud Sevice<br>2018-1'; //innerHTML  : HTML 을 찾아가서 변경하겠다.
+myHeading.innerHTML = 'Cloud Service<br>2018-1';
+function getIdPw() {
+    var id = prompt('ID 입력', '');
+    alert(id + '가 로그인함.');
+    var passwordSystem = '12345';
+    var password = prompt('PW 입력', '');
+    if (password === passwordSystem) alert('정상 로그인!');
+    else alert('비밀번호 오류');
+}
 
-var text = 'This is a text';
-text = 18.5;
-var a = 33.7;
-var b = a*text;
-var iceCream = 'chocolate';
-var id  = prompt('ID 입력', '');
+document.querySelector('h1').onclick = function () {
+    getIdPw();
+}
+
+var myImg = document.querySelector('img');
+myImg.onclick = function () {
+    var src = myImg.getAttribute('src');
+    if (src === 'images/firefox-icon.png')
+        myImg.setAttribute('src', 'images/mokwon.png');
+    else
+        myImg.setAttribute('src', 'images/firefox-icon.png');
+}
