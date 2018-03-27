041b8f5f21d71c529333c0d88413361ff19b569d
jaeoh <wodh4654@naver.com>
2018-03-27 17:00:28 +0900

    parkjaeoh

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,32 @@
 var myHeading = document.querySelector('h1');
-myHeading.innerHTML = 'Cloud Service<br>2018-1';
-function getIdPw() {
-    var id = prompt('ID 입력', '');
-    alert(id + '가 로그인함.');
-    var passwordSystem = '12345';
-    var password = prompt('PW 입력', '');
-    if (password === passwordSystem) alert('정상 로그인!');
-    else alert('비밀번호 오류');
-}
+var id = localStorage.getItem('id');
 
-document.querySelector('h1').onclick = function () {
-    getIdPw();
-}
 
-var myImg = document.querySelector('img');
-myImg.onclick = function () {
-    var src = myImg.getAttribute('src');
-    if (src === 'images/firefox-icon.png')
-        myImg.setAttribute('src', 'images/mokwon.png');
-    else
-        myImg.setAttribute('src', 'images/firefox-icon.png');
+
+function getIDPW(){
+  var id =prompt('ID 입력','');
+  var password = prompt(id + '가 사용할 초기 비번 입력','');
+  localStorage.setItem('id',id);
+  localStorage.setItem('password',password);
+  }
+
+
+
+  
+  var passwordSystem = '1234'
+var password = prompt('PW 입력','')
+if (password ===passwordSystem){
+    alert('로그인 성공')
+    var id = localStorage.getItem('id');
+var heading= document.querySelector('h1');
+heading.innerHTML = id + 'HomePage';
+    
 }
+else alert('로그인 실패')
+       
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+    myButton.onclick = function() {
+      getIDPW();
+    }  
